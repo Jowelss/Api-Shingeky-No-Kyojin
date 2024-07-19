7af268be54e86e5e2f735fbc43b5986c0be3842b
Jowelss <devejowels.gitl.loper@gmail.com>
2024-07-18 21:57:44 -0400

    modify fuctions asyncs, create fuctions for paint DOM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+const titansContainer = document.querySelector('.titans-container');
+const charactContainer = document.querySelector('.character-container');
+const templateTitans = document.getElementById('template-titans');
+const templateCharact = document.getElementById('template-characters');
+const fragment = document.createDocumentFragment();
+
 const getData = async () => {
   try {
     const response = await fetch('https://snk-player-api.vercel.app/');
@@ -18,30 +24,46 @@ const getApis = async () => {
 
     const { characters, titans } = await functPromise;
 
-    async function getCharact() {
-      const resCharact = await fetch(characters);
-      const dataCharact = await resCharact.json();
-
-      const { data } = dataCharact;
-      console.log(data);
-      return data;
-    }
-
-    await getCharact();
-
-    async function getTitan() {
-      const resTitan = await fetch(titans);
-      const dataTitan = await resTitan.json();
-
-      const { data } = dataTitan;
-      console.log(data);
-      return data;
-    }
-
-    await getTitan();
+    await Promise.all([getCharact(characters), getTitan(titans)]);
   } catch (error) {
     console.log(error);
   }
 };
 
 getApis();
+
+const getCharact = async (urlCharact) => {
+  try {
+    const resCharact = await fetch(urlCharact);
+
+    const dataCharact = await resCharact.json();
+
+    const { data } = await dataCharact;
+
+    addTitanDom(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addTitanDom = (arrTitan) => {
+  console.log(arrTitan);
+};
+
+const getTitan = async (urlTitan) => {
+  try {
+    const resTitan = await fetch(urlTitan);
+
+    const dataTitan = await resTitan.json();
+
+    const { data } = await dataTitan;
+
+    addCharactDom(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addCharactDom = (arrTitan) => {
+  console.log(arrTitan);
+};
